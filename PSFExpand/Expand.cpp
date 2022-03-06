4acc956b453d452d41717163c467bfd1c2d9c1f0
Lourdle <79350227+Lourdle@users.noreply.github.com>
2022-03-06 12:15:21 +0800

    Optimization: added long file names support

--- PSFExpand/Expand.cpp
+++ PSFExpand/Expand.cpp
@@ -47,10 +47,113 @@ static bool IsAnEmptyDirectory(wstring Dir)
 	return !ret;
 }
 
-static wstring MakePsfPathString(PCWSTR pCab, PCWSTR pCabFile)
+inline
+static bool IsUNCName(PCWSTR pPathName)
+{
+	if (wcslen(pPathName) >= 2)
+		if (pPathName[0] == '\\' && pPathName[1] == '\\')
+			return true;
+		else
+			return false;
+	else
+		return false;
+}
+
+static wstring FullPathName(PCWSTR pPathName)
+{
+	DWORD Length = GetFullPathNameW(pPathName, 0, nullptr, nullptr);
+	if (!Length)
+		return L"";
+
+	wstring Name;
+	Name.resize(Length - 1);
+
+	GetFullPathNameW(pPathName, Length, const_cast<LPWSTR>(Name.c_str()), nullptr);
+	return Name;
+}
+
+static wstring ShortPathName(PCWSTR pPathName)
+{
+	wstring FullName = FullPathName(pPathName);
+	if (FullName == L"")
+		return L"";
+	else
+		pPathName = FullName.c_str();
+
+	wstring Name = L"\\\\?\\";
+	DWORD Length;
+	PCWSTR pLongName;
+	bool unc = false;;
+
+	if (wcsncmp(pPathName, L"\\\\?\\", 4))
+	{
+		if (IsUNCName(pPathName))
+		{
+			Name += L"UNC\\";
+			Name += pPathName;
+			unc = true;
+		}
+		else
+			Name += pPathName;
+
+		pLongName = Name.c_str();
+	}
+	else
+		pLongName = pPathName;
+
+	wstring ShortName;
+	Length = GetShortPathNameW(pLongName, nullptr, 0);
+	if (!Length)
+		return L"";
+	ShortName.resize(Length - 1);
+	GetShortPathNameW(pLongName, const_cast<LPWSTR>(ShortName.c_str()), Length);
+	ShortName.erase(ShortName.cbegin(), ShortName.cbegin() + (unc ? 8 : 4));
+	return ShortName;
+}
+
+static wstring LongPathName(PCWSTR pPathName)
+{
+	wstring FullName = FullPathName(pPathName);
+	bool unc = false;
+	if (FullName == L"")
+		return L"";
+	else if (wcsncmp(FullName.c_str(), L"\\\\?\\", 4))
+	{
+		wstring tmp = L"\\\\?\\";
+		if (IsUNCName(pPathName))
+		{
+			tmp += L"UNC\\";
+			unc = true;
+		}
+
+		tmp.swap(FullName);
+		FullName += tmp;
+	}
+	pPathName = FullName.c_str();
+	DWORD Length;
+	Length = GetLongPathNameW(pPathName, nullptr, 0);
+	if (!Length)
+		return L"";
+
+	wstring Name;
+	Name.resize(Length - 1);
+
+	GetLongPathNameW(pPathName, const_cast<LPWSTR>(Name.c_str()), Length);
+	Name.erase(Name.cbegin(), Name.cbegin() + (unc ? 8 : 4));
+	return Name;
+}
+
+static wstring MakePsfPathString(PCWSTR pCabFile)
 {
 	wstring Psf;
 	Psf = pCabFile;
+	PCWSTR pCab = pCabFile + wcslen(pCabFile);
+	for (; pCab; pCab--)
+		if (*pCab == '\\')
+		{
+			++pCab;
+			break;
+		}
 
 	if (Screen(pCab, L"Windows*.*-KB???????-*_*.cab"))
 	{
@@ -96,28 +199,21 @@ static wstring MakePsfPathString(PCWSTR pCab, PCWSTR pCabFile)
 }
 
 inline
-bool PrintFileInfo(PCWSTR pCab, PCWSTR pCabFile, PCWSTR pPsfFile, PCWSTR pXmlFile, PCWSTR pOutDir)
+static bool PrintFileInfo(PCWSTR pCabFile, PCWSTR pPsfFile, PCWSTR pXmlFile, PCWSTR pOutDir)
 {
-	if (pCabFile)
-	{
-		wcout << L"CAB" << ' ' << GetString(FilePath) << '\n' << pCabFile << '\n';
-		if (!AccessFile(pCabFile))
-			return false;
-	}
-
 	wstring Psf;
 	if (!pPsfFile)
 	{
-		Psf = MakePsfPathString(pCab, pCabFile);
+		Psf = MakePsfPathString(pCabFile);
 		pPsfFile = Psf.c_str();
 	}
-	wcout << L"PSF" << ' ' << GetString(FilePath) << '\n' << pPsfFile << '\n';
+	wcout << L"PSF" << ' ' << GetString(FilePath) << '\n' << LongPathName(pPsfFile) << '\n';
 	if (!AccessFile(pPsfFile))
 		return false;
 
 	wcout << L"XML" << ' ' << GetString(FilePath) << '\n';
 	if (!pXmlFile)
-		wcout << pOutDir << L"\\express.psf.cix.xml" << '\n';
+		wcout << LongPathName(pOutDir) << L"\\express.psf.cix.xml" << '\n';
 	else
 	{
 		wcout << pXmlFile << '\n';
@@ -131,33 +227,51 @@ bool PrintFileInfo(PCWSTR pCab, PCWSTR pCabFile, PCWSTR pPsfFile, PCWSTR pXmlFil
 
 bool Expand(PCWSTR pCabFile, PCWSTR pPsfFile, PCWSTR pXmlFile, PCWSTR pOut, BYTE Flags)
 {
-	PWSTR pCab = nullptr;
-	unique_ptr<WCHAR[]> CabFile;
+	wstring CabFile;
 	if (pCabFile)
 	{
-		DWORD Length = GetFullPathNameW(pCabFile, 0, nullptr, nullptr);
-		if (!Length)
+		if (_wcsicmp(pCabFile + wcslen(pCabFile) - 4, L".cab"))
+		{
+			SetLastError(ERROR_BAD_FORMAT);
 			return false;
+		}
 
-		PWSTR pFile = new WCHAR[Length];
-		GetFullPathNameW(pCabFile, Length, pFile, &pCab);
+		CabFile = ShortPathName(pCabFile);
+		if (CabFile == L"")
+			return false;
+		pCabFile = CabFile.c_str();
 
-		CabFile.reset(pFile);
+		wcout << L"CAB" << ' ' << GetString(FilePath) << '\n' << LongPathName(pCabFile) << '\n';
 	}
 
 	wstring OutDir;
 	if (!pOut)
 	{
-		OutDir = CabFile.get();
-		OutDir.erase(OutDir.cbegin() + OutDir.size() - 4, OutDir.cend());
-
-		pOut = OutDir.c_str();
+		OutDir = LongPathName(CabFile.c_str());
+		OutDir.erase(OutDir.cend() - 4, OutDir.cend());
 	}
-	if (pCabFile && !CreateDirectoryW(pOut, nullptr))
-		if (GetLastError() != ERROR_ALREADY_EXISTS || !IsAnEmptyDirectory(OutDir))
+	else
+	{
+		OutDir = FullPathName(pOut);
+		if (OutDir == L"")
 			return false;
+	}
+	pOut = OutDir.c_str();
 
-	if (!PrintFileInfo(pCab, CabFile.get(), pPsfFile, pXmlFile, pOut))
+	if (pCabFile)
+	{
+		auto Out = wstring(L"\\\\?\\") + (IsUNCName(pOut) ? L"UNC\\" : L"");
+		Out += pOut;
+
+		if (!CreateDirectoryW(
+			Out.c_str(),
+			nullptr))
+			if (GetLastError() != ERROR_ALREADY_EXISTS
+				|| !IsAnEmptyDirectory(Out.c_str()))
+				return false;
+	}
+
+	if (!PrintFileInfo(pCabFile, pPsfFile, pXmlFile, pOut))
 		return false;
 
 	if (pCabFile)
@@ -179,9 +293,7 @@ bool Expand(PCWSTR pCabFile, PCWSTR pPsfFile, PCWSTR pXmlFile, PCWSTR pOut, BYTE
 		wstring CmdLine(L"Expand.exe ");
 		CmdLine += pCabFile;
 		CmdLine += L" -f:* ";
-		CmdLine += '\"';
-		CmdLine += pOut;
-		CmdLine += '\"';
+		CmdLine += ShortPathName(pOut).c_str();
 		PROCESS_INFORMATION pi;
 		if (!CreateProcessW(
 			nullptr,
@@ -220,16 +332,25 @@ bool Expand(PCWSTR pCabFile, PCWSTR pPsfFile, PCWSTR pXmlFile, PCWSTR pOut, BYTE
 	}
 
 	HPSF hPSF;
+	wstring XmlFile;
+	if (!pXmlFile)
+	{
+		XmlFile = ShortPathName(pOut);
+		if (XmlFile == L"")
+			return false;
+		XmlFile += L"\\express.psf.cix.xml";
+		pXmlFile = XmlFile.c_str();
+	}
 	if (!pPsfFile)
 	{
-		auto Psf = MakePsfPathString(pCab, CabFile.get());
+		auto Psf = MakePsfPathString(CabFile.c_str());
 		if (Psf == L"")
 			return false;
 
-		hPSF = PSFExtHandler_OpenFile(Psf.c_str(), pXmlFile ? pXmlFile : (wstring(pOut) + L"\\express.psf.cix.xml").c_str());
+		hPSF = PSFExtHandler_OpenFile(Psf.c_str(), pXmlFile);
 	}
 	else
-		hPSF = PSFExtHandler_OpenFile(pPsfFile, pXmlFile ? pXmlFile : (wstring(pOut) + L"\\express.psf.cix.xml").c_str());
+		hPSF = PSFExtHandler_OpenFile(pPsfFile, pXmlFile);
 
 	if (!hPSF)
 		return false;
